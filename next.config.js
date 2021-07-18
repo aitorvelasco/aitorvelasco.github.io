ce5448cf89a1891f7869a153e7b6243185f80fd7
Aitor Velasco <a.velascoo@hotmail.es>
2021-07-18 14:07:20 +0200

    continue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    env: {
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    },
     images: {
       loader: 'imgix',
       path: '',
