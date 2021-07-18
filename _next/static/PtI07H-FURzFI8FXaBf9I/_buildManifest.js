0296ad9cb912b40edd24e1df4ce101deaee41c1b
aitorvelasco <aitor.velasco@masmovil.com>
2021-07-18 13:46:38 +0000

    Deploying to gh-pages from @ aitorvelasco/aitorvelasco.github.io@f75307b09145196d221f668f073a41a8e8e45a42 🚀

--- _next/static/PtI07H-FURzFI8FXaBf9I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/686-eaf98e328c25420bb47c.js","static/chunks/pages/index-0bed349670973ff5017a.js"],"/_error":["static/chunks/pages/_error-94ed2348718d59e1af9b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/fRIzWwtbuIteG68r3SeYJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/686-eaf98e328c25420bb47c.js","static/chunks/pages/index-0e21a94026d6b84e94fe.js"],"/_error":["static/chunks/pages/_error-94ed2348718d59e1af9b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
