72b2818ac151487fd0afaf35eb3755b9287e100f
Aitor Velasco <aitorvelasco@MacBook-Pro-de-Aitor.local>
2021-09-19 17:48:09 +0200

    fix bp

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,27 @@ import { Container, Grid, Hidden, makeStyles } from '@material-ui/core'
 
 import { Aside, Curriculum, Footer, Header } from '../src/components'
 
-const useStyles = makeStyles(({ palette }) => ({
+const useStyles = makeStyles(({ spacing, palette, breakpoints }) => ({
   wrapper: {
     boxShadow: '-10px 10px',
     backgroundColor: palette.common.white,
     padding: 0,
     overflow: 'hidden',
   },
+  container: {
+    [breakpoints.down('xs')]: {
+      paddingLeft: spacing(1.5),
+      paddingRight: spacing(1.5),
+    },
+  },
 }))
 
 const Home = () => {
-  const { wrapper } = useStyles()
+  const { wrapper, container } = useStyles()
   return (
     <Container maxWidth="lg" className={wrapper}>
       <Header />
-      <Container maxWidth={false}>
+      <Container maxWidth={false} className={container}>
         <Grid container spacing={4}>
           <Hidden mdDown>
             <Grid item xs={2}>
--- src/theme/breakpoints.ts
+++ src/theme/breakpoints.ts
@@ -2,7 +2,7 @@ import createBreakpoints from '@material-ui/core/styles/createBreakpoints'
 
 const breakpoints = createBreakpoints({
   values: {
-    xs: 380,
+    xs: 360,
     sm: 540,
     md: 768,
     lg: 992,
