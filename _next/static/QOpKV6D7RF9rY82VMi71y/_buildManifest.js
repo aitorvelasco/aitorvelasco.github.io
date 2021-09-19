8367bad674896cb3a2545e9679ce235080cec7ac
aitorvelasco <aitor.velasco@masmovil.com>
2021-09-19 22:33:50 +0000

    Deploying to gh-pages from @ aitorvelasco/aitorvelasco.github.io@3dad06529ac2d1035cd449e242c0a3cf27686e86 🚀

---
+++ _next/static/QOpKV6D7RF9rY82VMi71y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/295-d74523b1fac162edd7b6.js","static/chunks/pages/index-5988bac6a28a9222d5a4.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
