40257eb42c414465e015a483752eb80d66b9d712
aitorvelasco <avelascoo92@gmail.com>
2022-10-30 06:44:26 +0100

    emotion server

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,58 +1,97 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheets } from '@mui/styles'
+import createEmotionServer from '@emotion/server/create-instance'
+import { createEmotionCache } from '../src/helpers'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheets = new ServerStyleSheets()
-    const originalRenderPage = ctx.renderPage
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-      })
-
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return {
-      ...initialProps,
-      styles: [
-        ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement(),
-      ],
-    }
-  }
-
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head>
-          <meta
-            name="description"
-            content="Portfolio y Curriculum Vitae de Aitor Velasco - Frontend Developer"
-          />
-          <meta property="og:title" content="Aitor Velasco | Front End Developer" />
-          <meta property="og:description" content="Portfolio y Curriculum Vitae de Aitor Velasco - Frontend Developer" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://aitorvelasco.github.io" />
-          <meta
-            property="og:image"
-            content="https://aitorvelasco.github.io/logo.jpg"
-          />
+      <Head>
+        <meta
+          name="description"
+          content="Portfolio y Curriculum Vitae de Aitor Velasco - Frontend Developer"
+        />
+        <meta property="og:title" content="Aitor Velasco | Front End Developer" />
+        <meta property="og:description" content="Portfolio y Curriculum Vitae de Aitor Velasco - Frontend Developer" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://aitorvelasco.github.io" />
+        <meta
+          property="og:image"
+          content="https://aitorvelasco.github.io/logo.jpg"
+        />
 
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Jomhuria&family=Josefin+Sans&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Main />
-        <NextScript />
-      </Html>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jomhuria&family=Josefin+Sans&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Main />
+      <NextScript />
+    </Html>
     )
   }
 }
 
-export default MyDocument
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const originalRenderPage = ctx.renderPage
+
+  // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents Emotion to render invalid HTML.
+  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  }
+}
+
--- src/helpers/core.helpers.ts
+++ src/helpers/core.helpers.ts
@@ -1,3 +1,2 @@
 export const environment = process.env.NODE_ENV
-
 export const isProduction = () => environment === 'production'
