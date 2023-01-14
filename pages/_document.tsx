43ed863be393e179001d87a6dae18f1334e243d8
aitor_velasco <a.velascoo@hotmail.es>
2023-01-14 17:26:09 +0100

    update deps and fix og:image url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
         <meta property="og:url" content="https://aitorvelasco.github.io" />
         <meta
           property="og:image"
-          content="https://aitorvelasco.github.io/logo.jpg"
+          content="https://aitorvelasco.github.io/logo-t.webp"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
