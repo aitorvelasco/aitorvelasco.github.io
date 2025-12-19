2d712def1525f9835d99460dbe23591ffb8b859c
aitorvelasco <avelascoo92@gmail.com>
2025-12-19 15:21:10 +0100

    react19, next16, turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 const nextConfig = {
   output: 'export',
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    })
-
-    return config
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
   images: {
     unoptimized: true
